18748681056df48db36a2081e0b56365680c8525
21133 <21133@wc.school.nz>
2025-03-12 09:39:54 +1300

    I have added the ability to input into Sign Up and Login.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 from sqlite3 import Error
 
@@ -12,10 +12,19 @@ def render_homepage():
 
 @app.route('/sign')
 def render_sign_page():
-    return render_template('sign.html')
+    if request.method == 'POST':
+        fname = request.form.get('user_fname').title.strip()
+        lname = request.form.get('user_fname').title.strip()
+        email = request.form.get('user_email').lower().strip()
+        password = request.form.get('user_password')
+        password2 = request.form.get('user_password2')
 
 
-@app.route('/login')
+
+    return render_template('sign.html', methods=['POST', 'GET'])
+
+
+@app.route('/login', methods=['POST', 'GET'])
 def render_login_page():
     return render_template('login.html')
 
